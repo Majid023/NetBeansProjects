bf4d064c989a9a6cc357124d6726f7f2b9ce204a
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-07-15 12:13:05 +0530

    simple programe hello world

---
+++ SimpleJavaFx/src/javafx/FirstProgramme.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafx;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author DELL
+ */
+public class FirstProgramme extends Application {
+    
+   public static void main(String... args){
+        launch(args);
+   }
+  
+   @Override
+   public  void start(Stage stage){
+       Button btn = new Button();
+       btn.setText( "Hello World" );
+       btn.setOnAction( new EventHandler<ActionEvent>(){
+           @Override
+           public void handle(ActionEvent event) {
+             System.out.println( "Hello World" );  //To change body of generated methods, choose Tools | Templates.
+           }
+       
+       });
+       
+       StackPane root  = new StackPane();
+       root.getChildren().add(btn);
+       
+       Scene scene = new Scene(root , 400 , 400);
+       
+       stage.setTitle( "Hello WOrld!" );
+       stage.setScene(scene);
+       stage.show();
+       
+   }
+}
