c4bb4c9d5c6e5f72db4b83c70057c6d8be5ee784
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-08-16 20:18:37 +0530

    css use in javafx

---
+++ JavaFXwithcss/src/css/JavaFXWithCSS.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package css;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author DELL
+ */
+public class JavaFXWithCSS extends Application {
+    public static void main(String... args){
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+        
+        Parent root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("CSS");
+        
+        primaryStage.show();
+    }
+        
+}
